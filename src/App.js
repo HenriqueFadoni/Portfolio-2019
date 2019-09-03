0404f4c6e4562770276cbb0a377c0960973d0bb5
Henrique Fadoni <henrique.fadoni@blindside.pro>
2019-09-03 16:58:55 -0300

    Basic Set Up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Redirect, Route, Switch } from 'react-router-dom';
+
+import Home from './containers/Home/Home';
 
 function App() {
+  const routers = (
+    <Switch>
+      <Route path="/" component={Home} exact />
+    </Switch>
+  )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {routers}
     </div>
   );
 }
