38618ca4160a8d27015f97a04ddcd0f9957a561c
Henrique Fadoni <henrique.fadoni@blindside.pro>
2019-09-03 19:03:42 -0300

    Adjusting to Hide Video when Screen Width is bellow 500

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import VideoBackground from './VideoBackground/VideoBackground';
 
-const Home = () => (
-    <div className="home__container">
-       <VideoBackground />
-    </div>
-);
+const Home = () => {
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const isResizing = e => setScreenWidth(window.innerWidth);
+        window.addEventListener("resize", isResizing);
+
+        return () => {
+            window.removeEventListener("resize", isResizing);
+        };
+    }, []);
+
+    return (
+        <div className="home__container">
+            {screenWidth > 500 ? <VideoBackground /> : null}
+
+        </div>
+    )
+};
 
 export default Home;
--- src/containers/Home/VideoBackground/VideoBackground.js
+++ src/containers/Home/VideoBackground/VideoBackground.js
@@ -4,13 +4,13 @@ import videoMp4 from '../../../assets/backgroundVideo/MP4/background.mp4';
 import videoWebm from '../../../assets/backgroundVideo/WEBM/background.webm';
 
 const VideoBackground = () => (
-    <>
-        <video className="home__video--background" autoPlay muted loop>
+    <div className="home__video-background--container">
+        <video className="home__video-background--content" autoPlay muted loop>
             <source src={videoMp4} type="video/mp4" />
             <source src={videoWebm} type="video/webm" />
             Your Browser is not Supported!
         </video>
-    </>
+    </div>
 );
 
 export default VideoBackground;
