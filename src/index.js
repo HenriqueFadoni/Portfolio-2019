37ba2a525ca08d85abdda42d12c5b8290070ba06
Henrique Fadoni <henrique.fadoni@blindside.pro>
2019-09-05 21:26:08 -0300

    Setting Up Redux

--- src/index.js
+++ src/index.js
@@ -2,12 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// Redux
+import { Provider } from 'react-redux';
+import { 
+    createStore,
+    combineReducers 
+} from 'redux';
+// Routes
 import { BrowserRouter } from 'react-router-dom';
 
+const rootReducer = combineReducers({});
+
+const store = createStore(rootReducer);
+
 const app = (
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
