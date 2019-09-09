f94475ee662561bd854750e07972d4f5e0eab963
Henrique Fadoni <henrique.fadoni@blindside.pro>
2019-09-09 20:46:28 -0300

    Animating Quote Section

--- src/containers/About/Quote/Quote.js
+++ src/containers/About/Quote/Quote.js
@@ -2,12 +2,28 @@ import React from 'react'
 
 const Quote = () => (
 	<div className="about__quote--container">
-		<h1 className="color--white">
-			I do not write code professionally,
-		</h1>
-		<h1 className="color--white">
-			I bring dreams into reality
-		</h1>
+		<div className="about__quote--text">
+			<h1 className="color--white">
+				I do not write code professionally,
+			</h1>
+			<ul className="about__quote--list">
+				<li>
+					<h1 className="color--white">
+						I bring dreams to reality!
+					</h1>
+				</li>
+				<li>
+					<h1 className="color--white">
+						I create products!
+					</h1>
+				</li>
+				<li>
+					<h1 className="color--white">
+						I change lives!
+					</h1>
+				</li>
+			</ul>
+		</div>
 	</div>
 )
 
