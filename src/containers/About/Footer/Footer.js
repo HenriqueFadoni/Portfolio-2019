fe422d9fc42d363589618a5c1fa2df61fb09ad1f
Henrique Fadoni <henrique.fadoni@blindside.pro>
2019-09-22 16:04:18 -0300

    Creating a Footer Element

--- src/containers/About/Footer/Footer.js
+++ src/containers/About/Footer/Footer.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
+import Title from './Title/Title'
+import Icons from './Icons/Icons'
+
 const Footer = () => (
-  <footer>
-    <h3></h3>
-    
-  </footer>
+	<footer className="footer__container">
+		<Title />
+		<Icons />
+	</footer>
 )
 
 export default Footer
