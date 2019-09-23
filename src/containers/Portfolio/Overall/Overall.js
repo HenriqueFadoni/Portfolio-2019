763842cd22c10f19b90df1c7022e66a2b5d28b55
Henrique Fadoni <henrique.fadoni@blindside.pro>
2019-09-23 15:42:18 -0300

    Re-styling Pinned and creating Overall in Portfolio

---
+++ src/containers/Portfolio/Overall/Overall.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Overall = () => {
+	return (
+		<div className="overall__container">
+
+		</div>
+	)
+}
+
+export default Overall
--- src/containers/Portfolio/Portfolio.js
+++ src/containers/Portfolio/Portfolio.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import Pinned from './Pinned/Pinned'
+import Overall from './Overall/Overall'
 import Footer from '../../components/Footer/Footer'
 
 const Portfolio = () => {
 	return (
 		<div className="portfolio__container">
 			<Pinned />
+			<Overall />
 			<Footer />
 		</div>
 	)
