6269c34aa2cc828f2b67a1fc4b68fe78d67ff2a4
Henrique Fadoni <henrique.fadoni@blindside.pro>
2019-09-24 12:13:39 -0300

    Adding Title and content to the OtherProjects

--- src/containers/Portfolio/OtherProjects/OtherProjects.js
+++ src/containers/Portfolio/OtherProjects/OtherProjects.js
@@ -2,27 +2,21 @@ import React from 'react'
 
 import ProjectList from '../Pinned/ProjectList/ProjectList'
 import project1 from '../../../assets/images/project-1.png'
+import Title from './Title/Title'
 
 const OtherProjects = () => {
 	const projectList = [
-		{ name: 'PathFinder', tag: 'React', repoLink: project1 },
-		{ name: 'PathFinder', tag: 'React', repoLink: project1 },
-		{ name: 'PathFinder', tag: 'React', repoLink: project1 },
-		{ name: 'PathFinder', tag: 'React', repoLink: project1 },
-		{ name: 'PathFinder', tag: 'React', repoLink: project1 },
-		{ name: 'PathFinder', tag: 'React', repoLink: project1 },
-		{ name: 'PathFinder', tag: 'React', repoLink: project1 },
-		{ name: 'PathFinder', tag: 'React', repoLink: project1 },
-		{ name: 'PathFinder', tag: 'React', repoLink: project1 },
-		{ name: 'PathFinder', tag: 'React', repoLink: project1 },
-		{ name: 'PathFinder', tag: 'React', repoLink: project1 },
-		{ name: 'PathFinder', tag: 'React', repoLink: project1 },
-		{ name: 'PathFinder', tag: 'React', repoLink: project1 },
-		{ name: 'PathFinder', tag: 'React', repoLink: project1 },
+		{ name: 'Money Now', tag: 'React', repoLink: project1 },
+		{ name: 'Crypto Currency App', tag: 'React Native', repoLink: project1 },
+		{ name: 'NPM Library', tag: 'React', repoLink: project1 },
+		{ name: 'Cocktail Finder', tag: 'React Native', repoLink: project1 },
+		{ name: 'Burguer Builder', tag: 'React', repoLink: project1 },
+		{ name: 'DashBoard', tag: 'React', repoLink: project1 }
 	]
 
 	return (
 		<div className="overall__container">
+			<Title />
 			<ProjectList projectList={projectList} />
 		</div>
 	)
---
+++ src/containers/Portfolio/OtherProjects/Title/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Title = () => (
+	<div className="overall__title">
+		<h1>
+			Projects
+		</h1>
+	</div>
+)
+
+export default Title
