77cca71b4b6a17a497d171d067b95c5e7f597d30
Henrique Fadoni <henrique.fadoni@blindside.pro>
2019-09-24 21:27:24 -0300

    Creating Button effect on hover Project

--- src/containers/Portfolio/OtherProjects/OtherProjects.js
+++ src/containers/Portfolio/OtherProjects/OtherProjects.js
@@ -2,16 +2,19 @@ import React from 'react'
 
 import ProjectList from '../Pinned/ProjectList/ProjectList'
 import Title from './Title/Title'
+
 import project1 from '../../../assets/images/project-1.png'
 import project2 from '../../../assets/images/project-2.png'
+import project4 from '../../../assets/images/project-4.png'
+import project5 from '../../../assets/images/project-5.png'
 
 const OtherProjects = () => {
 	const projectList = [
 		{ name: 'Money Now', tag: 'React', repoLink: project2 },
 		{ name: 'Crypto Currency App', tag: 'React Native', repoLink: project1 },
-		{ name: 'NPM Library', tag: 'React', repoLink: project1 },
+		{ name: 'NPM Library', tag: 'React', repoLink: project4 },
 		{ name: 'Cocktail Finder', tag: 'React Native', repoLink: project1 },
-		{ name: 'Burguer Builder', tag: 'React', repoLink: project1 },
+		{ name: 'Burguer Builder', tag: 'React', repoLink: project5 },
 		{ name: 'DashBoard', tag: 'React', repoLink: project1 }
 	]
 
--- src/containers/Portfolio/Pinned/ProjectList/Project/Project.js
+++ src/containers/Portfolio/Pinned/ProjectList/Project/Project.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Project = props => (
-	<div className="portfolio__pinned--item">
-		<div className="portfolio__pinned--item-img">
-			<img 
-				src={props.repoLink} 
-				alt="Project Image"
-			/>
-		</div>
-		<div className="portfolio__pinned--item-text">
-			<h4>{props.name}</h4>
-			<div className="portfolio__pinned--item-tag">
-				<div className="portfolio__pinned--item-dot">&nbsp;</div>
-				<h4>{props.tag}</h4>
+const Project = props => {
+	const [show, setShow] = useState(false);
+
+	const onHover = () => setShow(true)
+	const onHoverLeave = () => setShow(false)
+
+	return (
+		<div className="portfolio__pinned--item">
+			<div
+				className="portfolio__pinned--item-img"
+				onMouseEnter={onHover}
+				onMouseLeave={onHoverLeave}
+			>
+				{
+					show ?
+						<div>
+							<button>A</button>
+						</div>
+						: null
+				}
+
+				<img src={props.repoLink} alt="Project Image" />
+			</div>
+			<div className="portfolio__pinned--item-text">
+				<h4>{props.name}</h4>
+				<div className="portfolio__pinned--item-tag">
+					<div className="portfolio__pinned--item-dot">&nbsp;</div>
+					<h4>{props.tag}</h4>
+				</div>
 			</div>
 		</div>
-	</div>
-)
+	)
+}
 
 export default Project
