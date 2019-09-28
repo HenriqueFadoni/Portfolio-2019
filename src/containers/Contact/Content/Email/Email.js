bbc6ea76f14952d18b583ed197aeafabb07478ba
Henrique Fadoni <henrique.fadoni@blindside.pro>
2019-09-28 09:35:52 -0300

    Adding content to e-mail and re-styling components Email/Schedule

--- src/containers/Contact/Content/Email/Email.js
+++ src/containers/Contact/Content/Email/Email.js
@@ -3,7 +3,12 @@ import React from 'react'
 const Email = () => {
   return (
     <div className="contact__content--email">
-      
+      <h1>Contact Me</h1>
+      <h3>henriquefadonii@gmail.com</h3>
+      <h3>+55 35 99808-4004</h3>
+      <h3>Rua Coronel José Garcia, 27</h3>
+      <h3>Pouso Alegre, Minas Gerais - Brazil</h3>
+      <h3>37550-007</h3>
     </div>
   )
 }
