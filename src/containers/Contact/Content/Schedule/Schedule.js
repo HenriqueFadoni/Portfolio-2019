be94ec9bcb92507c8e49ca50ff9e767dc3f61602
Henrique Fadoni <henrique.fadoni@blindside.pro>
2019-09-28 16:09:00 -0300

    Creating reference for calendar

--- src/containers/Contact/Content/Schedule/Schedule.js
+++ src/containers/Contact/Content/Schedule/Schedule.js
@@ -1,33 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons'
 
 const Schedule = () => {
-  const [isHover, setIsHover] = useState(false)
+	const [isHover, setIsHover] = useState(false)
+	const calendar = useRef(null)
 
-  const onHoverHandler = () => setIsHover(!isHover)
+	const onHoverHandler = () => setIsHover(!isHover)
 
-  return (
-    <div
-      className="contact__content--schedule"
-      onMouseOver={onHoverHandler}
-      onMouseOut={onHoverHandler}
-    >
-      <a
-        className={isHover ? null : "calendar-bounce"}
-        href="https://calendly.com/henriquefadoni"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <FontAwesomeIcon
-          color="white"
-          size="10x"
-          icon={faCalendarAlt}
-        />
-      </a>
-      <h1>Schedule an Interview Now!</h1>
-    </div>
-  )
+	const clickHandle = () => calendar.current.click()
+
+	return (
+		<div
+			className="contact__content--schedule"
+			onMouseOver={onHoverHandler}
+			onMouseOut={onHoverHandler}
+			onClick={clickHandle}
+		>
+			<a
+				className={isHover ? null : "calendar-bounce"}
+				href="https://calendly.com/henriquefadoni"
+				target="_blank"
+				rel="noopener noreferrer"
+				ref={calendar}
+			>
+				<FontAwesomeIcon
+					color="white"
+					size="10x"
+					icon={faCalendarAlt}
+				/>
+			</a>
+			<h1>Schedule an Interview Now!</h1>
+		</div>
+	)
 }
 
 export default Schedule
