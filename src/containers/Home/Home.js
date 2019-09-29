9ee1554af33909067a74b5f20930004a16656536
Henrique Fadoni <henrique.fadoni@blindside.pro>
2019-09-29 12:49:57 -0300

    Fix - Taking out Scrolling Down

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -6,41 +6,26 @@ import Text from './MainText/MainText'
 
 const Home = () => {
 	const [screenWidth, setScreenWidth] = useState(window.innerWidth)
-	const [nextPage, setNextPage] = useState(false)
-
-	const scrollHandler = () => {
-		if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-			setNextPage(true)
-		} else if (nextPage) {
-			setNextPage(false)
-		}
-	}
 
 	useEffect(() => {
 		const isResizing = e => setScreenWidth(window.innerWidth)
 		window.addEventListener('resize', isResizing)
-		window.addEventListener('scroll', scrollHandler)
 
 		return () => {
 			window.removeEventListener('resize', isResizing)
-			window.removeEventListener('scroll', scrollHandler)
 		}
 	}, [])
-
-	if (!nextPage) {
-		return (
-			<div className="home__container">
-				{
-					screenWidth > 500
-						? <VideoBackground />
-						: null
-				}
-				<Text />
-			</div>
-		)
-	} else {
-		return <Redirect from="/" to="/about" />
-	}
+	
+	return (
+		<div className="home__container">
+			{
+				screenWidth > 500
+					? <VideoBackground />
+					: null
+			}
+			<Text />
+		</div>
+	)
 }
 
 export default Home
