2cf1713c753d882ded2e64c5fa81d9d8f7d02ab9
Henrique de O. Fadoni <38730725+HenriqueFadoni@users.noreply.github.com>
2019-10-01 17:22:04 -0300

    Responsiviness - Making Contact Page Responsive

--- src/containers/Contact/MapWrapper/MapWrapper.js
+++ src/containers/Contact/MapWrapper/MapWrapper.js
@@ -1,18 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import GoogleMap from './GoogleMap/GoogleMap'
 
-const MapWrapper = () => (
-	<div style={{
+const MapWrapper = () => {
+	const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+
+	useEffect(() => {
+		const isResizing = e => setScreenWidth(window.innerWidth)
+		window.addEventListener('resize', isResizing)
+
+		return () => {
+			window.removeEventListener('resize', isResizing)
+		}
+	}, [])
+
+	const styleWrap = {
 		height: '100vh',
 		width: '100%',
 		position: 'relative',
 		marginTop: 'none',
 		marginBottom: 'none',
 		overflow: 'hidden'
-	}}>
-		<GoogleMap />
-	</div>
-)
+	}
+
+	const styleResponsive = {
+		display: 'none'
+	}
+
+	return (
+		<div style={
+			screenWidth < 1000
+				? styleResponsive
+				: styleWrap
+		}>
+			<GoogleMap />
+		</div>
+	)
+}
 
 export default MapWrapper
