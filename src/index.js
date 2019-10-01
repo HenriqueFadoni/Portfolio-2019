0779de7eec03870ed51916ec132a900c5f2d1492
Henrique de O. Fadoni <38730725+HenriqueFadoni@users.noreply.github.com>
2019-10-01 20:31:34 -0300

    Deleting Redux - Not Used

--- src/index.js
+++ src/index.js
@@ -2,25 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
-// Redux
-// import { Provider } from 'react-redux'
-// import { 
-//     createStore,
-//     combineReducers 
-// } from 'redux'
+
 // Routes
 import { BrowserRouter } from 'react-router-dom'
 
-// const rootReducer = combineReducers({});
-
-// const store = createStore(rootReducer);
-
 const app = (
-	// <Provider store={store}>
 	<BrowserRouter>
 		<App />
 	</BrowserRouter>
-	// </Provider>
 )
 
 ReactDOM.render(app, document.getElementById('root'))
