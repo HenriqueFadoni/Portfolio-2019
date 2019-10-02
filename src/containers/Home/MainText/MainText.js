dd6bf44a5117ac739b6f575fcf60cb56078a9939
Henrique Fadoni <henrique.fadoni@blindside.pro>
2019-10-02 09:14:04 -0300

    Fix - Adding Subtitle

--- src/containers/Home/MainText/MainText.js
+++ src/containers/Home/MainText/MainText.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-import SubTitle from './Subtitle/Subtitle'
+import Subtitle from './Subtitle/Subtitle'
 import Title from './Title/Title'
 
 const Text = () => (
 	<div className="home__text--container">
 		<Title />
-		<SubTitle />
+		<Subtitle />
 	</div>
 )
 
---
+++ src/containers/Home/MainText/Subtitle/Subtitle.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Subtitle = () => (
+	<div className="home__text--subtitle-container">
+		<h1 className="subtitle--pacifico color--white">
+			Not Another Random Software Developer.
+		</h1>
+	</div>
+)
+
+export default Subtitle
