ce3f1aaecbfc73ffac45184eba9845bf06695ba9
Henrique Fadoni <henrique.fadoni@blindside.pro>
2019-10-10 22:17:19 -0300

    Adding New Photo to About Page

--- src/components/About/AboutHeader/PersonalImage/PersonalImage.js
+++ src/components/About/AboutHeader/PersonalImage/PersonalImage.js
@@ -1,13 +1,7 @@
 import React from 'react'
 
-import HenriquePhoto from '../../../../assets/images/photo_me.jpg'
-
 const PersonalImage = () => (
 	<div className="about__header__image">
-		<img 
-			src={HenriquePhoto} 
-			alt="Henrique, Software Developer" 
-		/>
 	</div>
 )
 
